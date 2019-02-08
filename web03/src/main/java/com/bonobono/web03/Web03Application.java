16b272fcc9560c47ee1bd1b8f4ad3591d24169cd
Administrator <Administrator@smart000>
2019-02-08 10:38:17 +0900

    [이다은] sampleCRUD 프로젝트 초기화

---
+++ web03/src/main/java/com/bonobono/web03/Web03Application.java
@@ -0,0 +1,14 @@
+package com.bonobono.web03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web03Application.class, args);
+	}
+
+}
+
