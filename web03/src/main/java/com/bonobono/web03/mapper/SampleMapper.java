befd717f48e96f3f6dce0b3015a7e2ea38296085
Administrator <Administrator@smart000>
2019-02-08 15:01:49 +0900

    [이다은] SampleMapper.java 파일에 insert 메서드 선언 - 매개변수로 Sample 객체를 받는 insertSample 메서드 선언

--- web03/src/main/java/com/bonobono/web03/mapper/SampleMapper.java
+++ web03/src/main/java/com/bonobono/web03/mapper/SampleMapper.java
@@ -3,10 +3,13 @@ package com.bonobono.web03.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bonobono.web03.vo.Sample;
 
 @Mapper
 public interface SampleMapper {
 	List<Sample> selectSampleList();
+	
+	int insertSample(Sample sample);
 }
