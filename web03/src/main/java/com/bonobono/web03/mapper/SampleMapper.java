1bb846d00cf39db6b99f0c353039a758a5797c91
Administrator <Administrator@smart000>
2019-02-08 12:32:50 +0900

    [이다은] sampleMapper.xml 파일과 SampleMapper.java 파일 수정 - 전체 리스트 출력을 위해 SELECT 쿼리 작성 및 메서드 Mapping

--- web03/src/main/java/com/bonobono/web03/mapper/SampleMapper.java
+++ web03/src/main/java/com/bonobono/web03/mapper/SampleMapper.java
@@ -1,8 +1,12 @@
 package com.bonobono.web03.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.bonobono.web03.vo.Sample;
 
 @Mapper
 public interface SampleMapper {
-
+	List<Sample> selectSampleList();
 }
