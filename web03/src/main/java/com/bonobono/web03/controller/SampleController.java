51c430409f06470d6d287bb467da35472842fff8
Administrator <Administrator@smart000>
2019-02-08 15:52:59 +0900

    [이다은] SampleController.java 파일에 삭제 처리를 위한 GetMapping - removeSample메서드를 호출 - 삭제 처리 후 sampleList로 redirect

--- web03/src/main/java/com/bonobono/web03/controller/SampleController.java
+++ web03/src/main/java/com/bonobono/web03/controller/SampleController.java
@@ -18,7 +18,6 @@ public class SampleController {
 	// 1. 입력 폼
 	@GetMapping("/addSample")	// Get방식
 	public String addSample() {
-		
 		// 자동으로 view 파일로 forward
 		return "addSample";		// view 이름은 template폴더(생략)의 addSample.html
 	}
@@ -35,7 +34,7 @@ public class SampleController {
 		// 값이 setting된 객체를 addSample메서드에 매개변수 자리에 넣고 호출
 		int result = sampleService.addSample(sample);
 		// 메서드가 실행, 쿼리문이 실행되어 테이블에 값이 입력될 경우 1, 그렇지 않을 경우 0을 출력
-		System.out.println("쿼리 실행 여부 -> " + result);
+		System.out.println("입력 쿼리 실행 여부 -> " + result);
 		// /sampleList로 redirect
 		return "redirect:/sampleList";
 	}
@@ -43,12 +42,26 @@ public class SampleController {
 	// 3. 목록
 	@GetMapping("/sampleList")
 	public String sampleList(Model model) {
-		// list라는 이름으로 view에 forward
+		// list라는 이름으로 getSampleList메서드의 리턴값을 view에 forward
 		model.addAttribute("list", sampleService.getSampleList());
 		return "sampleList";
 	}
 	
 	// 4. 삭제
+	@GetMapping("/removeSample")
+	public String removeSample(@RequestParam(value="") int sampleId) {
+		// get방식으로 보낸 Parameter값을 매개변수로 받고 확인
+		System.out.println("sampleId -> " + sampleId);
+		// Sample객체를 생성하고 그 객체에 값을 setting
+		Sample sample = new Sample();
+		sample.setSampleId(sampleId);
+		// 값이 setting된 객체를 removeSample변수 매개변수 자리에 넣고 호출
+		int result = sampleService.removeSample(sample);
+		// 메서드가 실행되고 쿼리가 호출되면 리턴값이 1, 아닐경우 0
+		// 콘솔창에서 쿼리 실행 여부 확인 후 sampleList로 redirect
+		System.out.println("삭제 쿼리 실행 여부 -> " + result);
+		return "redirect:/sampleList";
+	}
 	
 	// 5. 수정 폼
 	
